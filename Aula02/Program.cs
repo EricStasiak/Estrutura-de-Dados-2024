a56f24945b489b424e8b70ca2de2ed108f989ae9
EricStasiak <eric.stasiak@unoesc.edu.br>
2024-08-28 21:55:45 -0300

    Add files via upload

---
+++ Aula02/Program.cs
@@ -0,0 +1,19 @@
+﻿using Aula02;
+
+Person person = new Person();
+Console.WriteLine(person.Name);
+
+Person person1 = new Person ("Eric Dobler Stasiak", 18);
+Console.WriteLine(person1.Name);
+
+Person person2 = new Person ();
+person2.Name = "Makoto Yuki";
+person2.Age = 16;
+Console.WriteLine(person2.Name);
+
+Person person3 = new Person ()
+{
+    Name = "Leonel Brizola",
+    Age = 80
+};
+Console.WriteLine(person3.Name);
