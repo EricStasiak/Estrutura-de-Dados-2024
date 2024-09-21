e45b8f3d705d58c0971a08b3a65972a3e2548395
EricStasiak <eric.stasiak@unoesc.edu.br>
2024-09-21 01:22:18 -0300

    Add files via upload

---
+++ Filas/CallCenter.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filas
+{
+    public class CallCenter
+    {
+        private int _counter = 0;
+        public Queue<IncomingCall>? Calls { get; set; }
+        public CallCenter()
+        {
+            Calls = new Queue<IncomingCall>();
+        }
+
+        // Metodo para abertura de chamados
+        public void Call(int clientId)
+        {
+            Calls!.Enqueue(call);
+            new IncomingCall()
+            {
+                Id = ++_counter,
+                ClientId = clientId,
+                CallTime = DateTime.Now
+            };
+        }
+
+    public IncomingCall Answer(string consultant)
+    {
+        //Validação: Verificar se tem atendimentos na fila
+        if(Calls!.Count > 0)
+        {
+            IncomingCall call = new Calls.Dequeue();
+            call.Consultant = consultant;
+            call.StartTime = DateTime.Now;
+
+            return call;
+        }
+
+        return null!;
+    }
+
+    public void End(IncomingCall call)
+    {
+        call.EndTime = DateTime.Now;
+    }
+    public bool AreWaitingCalls()
+}
+}
---
+++ GameMap/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        TerrainEnum[,] map =
+        {
+            { TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS },
+            { TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS },
+            { TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS },
+            { TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS },
+            { TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.WALL, TerrainEnum.WALL },
+            { TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER },
+            { TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS },
+            { TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS },
+            { TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.SAND, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS, TerrainEnum.GRASS },
+            { TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WALL, TerrainEnum.WATER, TerrainEnum.WATER },
+            { TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER, TerrainEnum.WATER }
+        };
+
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+
+        for (int row = 0; row < map.GetLength(0); row++)
+        {
+            for (int column = 0; column < map.GetLength(1); column++)
+            {
+                // Obtém a cor e o caractere do terreno
+                Console.ForegroundColor = map[row, column].GetColor();
+                Console.Write(map[row, column].GetChar() + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // Reseta a cor do console para a cor padrão
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+}
