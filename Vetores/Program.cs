796d477bca8f2150e0ce14c9bc4731e8d32d769b
EricStasiak <eric.stasiak@unoesc.edu.br>
2024-09-21 01:30:34 -0300

    Add files via upload

---
+++ Vetores/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] pares = new int[100];
+        int[] impares = new int[100];
+
+        int numPares = 0;
+        int numImpares = 0;
+        int numero = 1;
+        while (numPares < 100 || numImpares < 100)
+        {
+            if (numero % 2 == 0)
+            {
+                if (numPares < 100)
+                {
+                    pares[numPares] = numero;
+                    numPares++;
+                }
+            }
+            else
+            {
+                if (numImpares < 100)
+                {
+                    impares[numImpares] = numero;
+                    numImpares++;
+                }
+            }
+
+            numero++;
+        }
+        Console.WriteLine("Números Pares:");
+        foreach (int par in pares)
+        {
+            Console.Write(par + " ");
+        }
+
+        Console.WriteLine("\n\nNúmeros Ímpares:");
+        
+        foreach (int impar in impares)
+        {
+            Console.Write(impar + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
